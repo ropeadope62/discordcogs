07e663776f9b30105e6b7e71220e564bd85c0184
ropeadope62 <davechadwick@gmail.com>
2022-10-13 23:46:25 -0400

    hunting fix config

--- hunting/getguild.py
+++
@@ -1,7 +0,0 @@
-from discord import *
-from discord.ext import *
-client = Discord.Client();
-
-client.on('ready', () => {
-        client.guilds.forEach(guild => console.log(guild.id, guild.name))
-        };
