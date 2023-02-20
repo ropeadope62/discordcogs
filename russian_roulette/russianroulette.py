1be3cbec2ed2c23ac44dd26a30360def61eb6790
ropeadope62 <davechadwick@gmail.com>
2023-02-20 12:08:27 -0500

    Update russianroulette.py

--- russian_roulette/russianroulette.py
+++ russian_roulette/russianroulette.py
@@ -20,22 +20,17 @@ class FancyDictList(dict):
 
 class RussianRoulette(commands.Cog):
     def __init__(self, bot):
+        self.bot = bot
         self.config = Config.get_conf(self, 45465465488435321554, force_registration=True)
         self.players = FancyDictList()
-        self.round_started = FancyDict()
-        self.winners = FancyDictList()
-        self.dead = FancyDictList()
-        self.current_player = FancyDictList()
-        self.player_numbers = {}
+        self.active = FancyDict()
 
         guild_defaults = {
             "Wait": 20,
-            "Buyin": 1000,
-            "Prize": 0,
-            "playermaxcount": 6,
+            "Chambers": 0,
         }
         
-        member_defaults = {"Wins": 0, "Losses": 0}
+        member_defaults = {"Wins": 0, "Losses": 0, "Total Winnings": 0 }
 
         self.config.register_guild(**guild_defaults)
         self.config.register_member(**member_defaults)
@@ -52,144 +47,62 @@ class RussianRoulette(commands.Cog):
     async def russianroulette(self, ctx):
         pass
     @russianroulette.command()
-    async def start(self, ctx):
-        """Begins a new race.
-
-        You cannot start a new race until the active on has ended.
-
-        If you are the only player in the race, you will race against
-        your bot.
-
-        The user who started the race is automatically entered into the race.
+    async def russianroulette(self, ctx, opponent: discord.Member):
         """
-        if self.round_started[ctx.guild.id]:
-            return await ctx.send(f"A game is already in progress,  Type `{ctx.prefix}russianroullete join` to join!")
-        self.round_started[ctx.guild.id] = True
-        self.players[ctx.guild.id].append(ctx.author)
-        wait = await self.config.guild(ctx.guild).Wait()
-        await ctx.send(
-            f"🚩 A game of Russian Roulette has begun! Type {ctx.prefix}russianroulette join "
-            f"to join the game! 🚩\nThe round will begin in "
-            f"{wait} seconds!\n\n**{ctx.author.mention}** entered the game!"
-        )
+        Start a game of Russian Roulette with the specified opponent.
+        """
+        if self.active[ctx.guild.id]:
+            await ctx.send(f"A game of Russian Roulette is already in progress. Wait please.")
+            return 
+        if opponent == ctx.author:
+            await ctx.send("You can't play Russian Roulette with yourself!")
+            return
+        else: 
+            self.players[ctx.guild.id].append(ctx.author)
+            await ctx.send(f"{opponent.mention}, do you accept this game of Russian Roulette? Type 'yes' or 'no'.")
 
-        await asyncio.sleep(wait)
-        self.round_started[ctx.guild.id] = True
-        await ctx.send("🏁 A game is now in progress. 🏁")
-        await self.run_game(ctx)
+        def check(msg):
+            return msg.author == opponent and msg.content.lower() in ['yes', 'no']
 
-        settings = await self.config.guild(ctx.guild).all()
-        currency = await bank.get_currency_name(ctx.guild)
-        color = await ctx.embed_colour()
-        msg, embed = await self._build_end_screen(ctx, settings, currency, color)
-        await ctx.send(content=msg, embed=embed)
-        await self._race_teardown(ctx, settings)
+        try:
+            msg = await self.bot.wait_for('message', check=check, timeout=30.0)
+        except asyncio.TimeoutError:
+            await ctx.send(f"{opponent.mention} didn't respond in time. Game cancelled.")
+            return
 
-    @russianroulette.command()
-    async def stats(self, ctx, user: discord.Member = None):
-            """Display your russian roulette stats."""
-            if not user:
-                user = ctx.author
-            color = await ctx.embed_colour()
-            user_data = await self.config.member(user).all()
-            player_total = sum(user_data["Wins"].values()) + user_data["Losses"]
-            embed = discord.Embed(color=color, description="Race Stats")
-            embed.set_author(name=f"{user}", icon_url=user.avatar_url)
-            embed.add_field(
-                name="Wins",
-                value=(
-                    f"Wins: {user_data['Wins']}"
-                ),
-            )
-            embed.add_field(name="Losses", value=f'{user_data["Losses"]}')
-            await ctx.send(embed=embed)
-            
-    @russianroulette.command()
-    async def join(self, ctx):
-            """Join a game of Russian Roulette
+        if msg.content.lower() == 'no':
+            await ctx.send(f"{opponent.mention} declined the game. Game cancelled.")
+            return
 
-            """
-            if self.round_started[ctx.guild.id]:
-                return await ctx.send(
-                    "A game has already started, wait for the previous round to finish"
-                )
-            elif not self.round_started.get(ctx.guild.id):
-                return await ctx.send("A game must be started before you can enter.")
-            elif ctx.author in self.players[ctx.guild.id]:
-                return await ctx.send("You have already entered the game.")
-            elif len(self.players[ctx.guild.id]) >= 6:
-                return await ctx.send("The maximum number of players has been reached.")
-            else:
-                self.players[ctx.guild.id].append(ctx.author)
-                await ctx.send(f"{ctx.author.mention} has joined the game.")
+    # Both players accepted, randomly choose who goes first
+        self.started[ctx.guild.id] = True
+        self.players[ctx.guild.id].append(ctx.opponent)
+        random.shuffle(self.players)[ctx.guild.id]
+        await ctx.send(f"{self.players[ctx.guild.id][0].mention} goes first.")
 
-    @russianroulette.command(hidden=True)
-    @checks.admin_or_permissions(administrator=True)
-    async def clear(self, ctx):                                                     
-            """ONLY USE THIS COMMAND FOR DEBUGPURPOSES
+    # Set up the game with a bullet in one of the chambers
+        chambers = [0] * 6
+        chambers[random.randint(0, 5)] = 1
 
-            You shouldn't use this command unless the round is stuck
-            or you are debugging."""
-            self.clear_local(ctx)
-            await ctx.send("Round cleared.")
+        # Play the game until someone loses
+        current_player = self.players[ctx.guild.id][0]
+        while True:
+        # Ask the player to pull the trigger
+            await ctx.send(f"{current_player.mention}, pull the trigger! Type 'pull' to pull the trigger.")
 
-    @russianroulette.command()
-    @checks.is_owner()
-    async def wipe(self, ctx):
-            """This command will wipe ALL race data.
+            def check(msg):
+                return msg.author == current_player and msg.content.lower() == 'pull'
 
-            You are given a confirmation dialog when using this command.
-            If you decide to wipe your data, all stats and settings will be deleted.
-            """
-            await ctx.send(
-                f"You are about to clear all russianroulette data including stats and settings. "
-                f"If you are sure you wish to proceed, type `{ctx.prefix}yes`."
-            )
-            choices = (f"{ctx.prefix}yes", f"{ctx.prefix}no")
-            check = lambda m: (m.author == ctx.author and m.channel == ctx.channel and m.content in choices)
             try:
-                choice = await ctx.bot.wait_for("message", timeout=20.0, check=check)
+                msg = await bot.wait_for('message', check=check, timeout=30.0)
             except asyncio.TimeoutError:
-                return await ctx.send("No response. russian roulette wipe cancelled.")
+                await ctx.send(f"{current_player.mention} didn't respond in time. Game cancelled.")
+                return
 
-            if choice.content.lower() == f"{ctx.prefix}yes":
-                await self.config.guild(ctx.guild).clear()
-                await self.config.clear_all_members(ctx.guild)
-                return await ctx.send("russian roulette data has been wiped.")
-            else:
-                return await ctx.send("russian roulette wipe cancelled.")
-            
-    @russianroulette.command()
-    async def version(self, ctx):
-            """Displays the version of russian roulette."""
-            await ctx.send(f"You are running russian roulette version {__version__}.")
+        # Check if the player pulled the unlucky bullet
+            if chambers.pop(0) == 1:
+                await ctx.send(f"{current_player.mention} pulled the trigger and the gun fired! You lose!")
+            break
 
-    @commands.group()
-    @commands.guild_only()
-    @checks.admin_or_permissions(administrator=True)
-    async def setrussianroulette(self, ctx):
-        """Race settings commands."""
-        pass
-
-
-    @setrussianroulette.command()
-    async def wait(self, ctx, wait: int):
-            """Changes the wait time before a game starts.
-
-            This only affects the period where game is still waiting
-            for more participants to join the game."""
-            if wait < 0:
-                return await ctx.send("Really? You're an idiot.")
-            await self.config.guild(ctx.guild).Wait.set(wait)
-            await ctx.send(f"Wait time before a game begins is now {wait} seconds.")
-
-    @checks.is_owner()
-    @setrussianroulette.group(name="buyin")
-    async def buyin(self, ctx, amount: int ):
-            """Buyin."""
-            await self.config.guild(ctx.guild).Buyin.set(amount)
-            await ctx.send(f"Buy in amount set to {amount}.")
-
-
-
-    
+        # Switch to the next player
+        current_player = self.players[ctx.guild.id][1] if current_player == self.players[ctx.guild.id][0] else self.players[ctx.guild.id][0]
