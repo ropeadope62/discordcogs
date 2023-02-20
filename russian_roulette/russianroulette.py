f6a278c2d26d6a7a0388329abcb7d6ce526bc45c
ropeadope62 <davechadwick@gmail.com>
2023-02-20 13:59:22 -0500

    Update russianroulette.py

--- russian_roulette/russianroulette.py
+++ russian_roulette/russianroulette.py
@@ -96,7 +96,7 @@ class RussianRoulette(commands.Cog):
                 return msg.author == current_player and msg.content.lower() == 'pull'
 
             try:
-                msg = await self.wait_for('message', check=check, timeout=30.0)
+                msg = await self.bot.wait_for('message', check=check, timeout=30.0)
             except asyncio.TimeoutError:
                 await ctx.send(f"{current_player.mention} didn't respond in time. Game cancelled.")
                 return
