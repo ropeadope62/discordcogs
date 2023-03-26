92d7332e769094f1c4b968fb24606798925bb576
ropeadope62 <davechadwick@gmail.com>
2023-03-26 11:34:21 -0400

    Update post_mortem.py

--- post_mortem/post_mortem.py
+++ post_mortem/post_mortem.py
@@ -168,7 +168,7 @@ class PostMortem(commands.Cog):
                 description="*Final Report Summary*",
                 color=discord.Color.dark_red(),
             )
-            embed.set_author(name=user, icon_url=ctx.user.avatar_url)
+            embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
             embed.add_field(name="Subject", value=user.mention, inline=False)
             embed.add_field(name="Death Year", value=f"{death_year}", inline=False)
             embed.add_field(
