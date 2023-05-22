61189b89233fd790e841ada6f9d59fbd079f70d1
ropeadope62 <davechadwick@gmail.com>
2023-05-22 14:45:48 -0400

    fix

--- post_mortem/reportembeds.py
+++ post_mortem/reportembeds.py
@@ -8,29 +8,53 @@ class ReportEmbeds:
         self.embed = discord.Embed(title="**Broad Street Labs™ - Post Mortem®**",description="*Final Report Summary*",color=discord.Color.dark_red())
 
     def report_embed(self):
-        self.embed.set_author(name=self.user.display_name, icon_url=self.user.display_avatar.url)
-        self.embed.add_field(name="Subject", value=self.user.mention, inline=False)
-        self.embed.add_field(name="Death Progress", value=f"{self.user_data['progress_bar']} {self.user_data['progress'] * 100:.1f}%", inline=False)
-        self.embed.add_field(name="Subject Risk Factors", value=f"{self.user_data['risk_factor']}", inline=False)
-        self.embed.add_field(name="Approximate Age", value=f"{self.user_data['approximate_age']}", inline=False)
-        self.embed.add_field(name="Death Year", value=f"{self.user_data['death_year']}", inline=False)
-        self.embed.add_field(name="Approximate Death Age", value=f"{self.user_data['approximate_death_age']}", inline=False)
-        self.embed.add_field(name="Time Left", value=f"{self.user_data['years_left']} years... or {self.user_data['months_left']} months... or {self.user_data['weeks_left']} weeks... or {self.user_data['days_left']} days left to live.", inline=False)
-        self.embed.add_field(name="** Post Mortem® Likely result of death:**", value=f"*{self.user_data['cause_of_death']}*",inline=False,)
-        self.embed.set_footer(text="\n Sponsored by Empties")
-        return self.embed
+        return self._extracted_from_report_initial_embed_2()
     
     def report_initial_embed(self): 
         
-        self.embed.set_author(name=self.user.display_name, icon_url=self.user.display_avatar.url)
+        return self._extracted_from_report_initial_embed_2()
+
+    # TODO Rename this here and in `report_embed` and `report_initial_embed`
+    def _extracted_from_report_initial_embed_2(self):
+        self.embed.set_author(
+            name=self.user.display_name, icon_url=self.user.display_avatar.url
+        )
         self.embed.add_field(name="Subject", value=self.user.mention, inline=False)
-        self.embed.add_field(name="Death Progress", value=f"{self.user_data['progress_bar']} {self.user_data['progress'] * 100:.1f}%", inline=False)
-        self.embed.add_field(name="Subject Risk Factors", value=f"{self.user_data['risk_factor']}", inline=False)
-        self.embed.add_field(name="Approximate Age", value=f"{self.user_data['approximate_age']}", inline=False)
-        self.embed.add_field(name="Death Year", value=f"{self.user_data['death_year']}", inline=False)
-        self.embed.add_field(name="Approximate Death Age", value=f"{self.user_data['approximate_death_age']}", inline=False)
-        self.embed.add_field(name="Time Left", value=f"{self.user_data['years_left']} years... or {self.user_data['months_left']} months... or {self.user_data['weeks_left']} weeks... or {self.user_data['days_left']} days left to live.", inline=False)
-        self.embed.add_field(name="** Post Mortem® Likely result of death:**", value=f"*{self.user_data['cause_of_death']}*",inline=False,)
+        self.embed.add_field(
+            name="Death Progress",
+            value=f"{self.user_data['progress_bar']} {self.user_data['progress'] * 100:.1f}%",
+            inline=False,
+        )
+        self.embed.add_field(
+            name="Subject Risk Factors",
+            value=f"{self.user_data['risk_factor']}",
+            inline=False,
+        )
+        self.embed.add_field(
+            name="Approximate Age",
+            value=f"{self.user_data['approximate_age']}",
+            inline=False,
+        )
+        self.embed.add_field(
+            name="Death Year",
+            value=f"{self.user_data['death_year']}",
+            inline=False,
+        )
+        self.embed.add_field(
+            name="Approximate Death Age",
+            value=f"{self.user_data['approximate_death_age']}",
+            inline=False,
+        )
+        self.embed.add_field(
+            name="Time Left",
+            value=f"{self.user_data['years_left']} years... or {self.user_data['months_left']} months... or {self.user_data['weeks_left']} weeks... or {self.user_data['days_left']} days left to live.",
+            inline=False,
+        )
+        self.embed.add_field(
+            name="** Post Mortem® Likely result of death:**",
+            value=f"*{self.user_data['cause_of_death']}*",
+            inline=False,
+        )
         self.embed.set_footer(text="\n Sponsored by Empties")
         return self.embed
     
