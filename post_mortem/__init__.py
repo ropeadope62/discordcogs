0e6865ba249abf9aa559928a0005987907f99531
ropeadope62 <davechadwick@gmail.com>
2023-05-22 17:32:44 -0400

    structure

--- post_mortem/__init__.py
+++ post_mortem/__init__.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 
-from .post_mortem import PostMortem
+from .postmortem import PostMortem
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
