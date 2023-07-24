db5b4210c1e134921542ebd6ef37adc5315f77dc
ropeadope62 <davechadwick@gmail.com>
2023-07-24 11:59:11 -0400

    new cog

---
+++ paurymovic/__init__.py
@@ -0,0 +1,28 @@
+import aiohttp
+from redbot.core.bot import Red
+from .paurymovic import PauryMovic
+from redbot.core.data_manager import cog_data_path
+import os
+
+async def download_file(url, filename):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                with open(filename, 'wb') as f:
+                    f.write(await resp.read())
+
+async def setup(bot):
+    cog = PauryMovic(bot)
+    await bot.add_cog(cog)
+
+    cog_folder = str(cog_data_path(cog))
+
+    files = {
+        "https://1drv.ms/u/s!ArCyJDmwlibnob8ztJQYgM5Yi50mag?e=cvWTRP": os.path.join(cog_folder, "GothamBold.ttf"),
+        "https://i.postimg.cc/pTP54jpF/you_are_the_father.png": os.path.join(cog_folder, "you_are_the_father.png"),
+        "https://i.postimg.cc/pTP54jpF/you_arenot_thefather.png": os.path.join(cog_folder, "you_arenot_thefather.png")
+    }
+
+    for url, file_path in files.items():
+        if not os.path.exists(file_path):
+            await download_file(url, file_path)
---
+++ paurymovic/paurymovic.py
@@ -0,0 +1,103 @@
+import discord
+from redbot.core import commands
+from redbot.core.data_manager import cog_data_path
+import random
+from datetime import timedelta, datetime
+from typing import List
+from PIL import Image, ImageDraw, ImageFont
+import requests
+from io import BytesIO
+import textwrap
+import os
+
+class PauryMovic(commands.Cog):
+    """**Welcome to Broad Street Labs:tm: - PauryMovic DNA Test:registered:**"""
+
+    __author__ = ["ropeadpope62"]
+    __version__ = "0.1.0"
+
+    results: List[str] = [
+        "you ARE the father!",
+        "you are NOT the father!",
+    ]
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    def format_help_for_context(self, ctx: commands.Context) -> str:
+        """
+        Thanks for using Broadstreet Labs DNA Test!
+        """
+        pre_processed = super().format_help_for_context(ctx)
+        return f"{pre_processed}\n\nCog Version: {self.__version__}"
+
+    async def red_delete_data_for_user(self, **kwargs):
+        """
+        Nothing to delete
+        """
+        return
+    
+    
+
+    @commands.command()
+    @commands.cooldown(1, 60, commands.BucketType.user)
+    async def paurymovic(
+        self, ctx: commands.Context, user1: discord.Member, user2: discord.Member
+    ):
+        """
+        The Paury Movic test will determine if the first user is the father of the second user.
+
+        Example: `>paurymovic @user1 @user2`
+        """
+        try:
+            if user1 is None or user2 is None:
+                help_menu = (
+                    "**Paury Movic Help Menu**\n"
+                    "To use the command, mention two users.\n"
+                    "Example: `>paurymovic @user1 @user2`\n\n"
+                    "This test will determine if @user1 is the father of @user2.\n"
+                )
+                await ctx.send(help_menu)
+                return
+        
+            img_path_father = os.path.join(str(cog_data_path(self)), 'you_are_the_father.png')
+            img_path_not_father = os.path.join(str(cog_data_path(self)), 'you_arenot_thefather.png')
+            img_path_result = os.path.join(str(cog_data_path(self)), 'pm_result.png')
+            font_path = os.path.join(str(cog_data_path(self)), 'GothamBold.ttf')
+
+            account_age_difference = user1.created_at - user2.created_at
+
+            if account_age_difference < timedelta(days=2 * 365):  # 8 years difference
+                test_result = self.results[1]
+                img_url = img_path_not_father
+            else:
+                test_result = self.results[0]
+                img_url = img_path_father
+
+            # Fetch the template image
+            img = Image.open(img_path_result)
+
+            draw = ImageDraw.Draw(img)
+            font = ImageFont.truetype(font_path, 24)
+            text = (f"The results are in.. {user1.display_name}, when it comes to {user2.display_name}, {test_result}")
+            wrapper = textwrap.TextWrapper(width=22)  # Replace 40 with the actual maximum width
+            lines = wrapper.wrap(text=text)
+            y_text = 450
+            for line in lines:
+                draw.text((425, y_text), line, fill= (58, 40, 100) , font=font, )
+                y_text += 30
+            img.save(img_path_result)
+
+            await ctx.send(
+                file=discord.File(img_path_result, filename="pm_result.png")
+            )
+
+        except Exception as e:
+            await ctx.send(f"An error occurred: {str(e)}")
+
+    @paurymovic.error
+    async def paurymovic_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            await ctx.send('Please wait for 1 minute before submitting another DNA test.')
+        else:
+            await ctx.send(f"An error occurred: {str(error)}")
