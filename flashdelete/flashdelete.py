768ffe8210d4dd23f2dc67dc6ae00359ccb97c3f
ropeadope62 <davechadwick@gmail.com>
2023-07-30 21:17:23 -0400

    Update flashdelete.py

--- flashdelete/flashdelete.py
+++ flashdelete/flashdelete.py
@@ -9,6 +9,7 @@ class FlashDelete(commands.Cog):
         self.bot = bot
         self.enabled = False
         self.timer = 5
+        self.bot.loop.create_task(self.delete_messages())
 
     def read_channel_ids(self):
         with open(".\\flashdelete_channels.json", "r") as file:
@@ -50,33 +51,37 @@ class FlashDelete(commands.Cog):
         """Run Flash Delete Now"""
         await self.delete_messages()
 
-    async def delete_messages(self):
+    async def flashdelete_task(self):
         while True:
             if self.enabled:
-                deleted_messages_count = 0
-                # for channel_id in self.read_channel_ids(): For use with json, we will use the hardcoded list for now
-                for channel_id in [1020933443179253800, 1130587895665807371]:
-                    channel = self.bot.get_channel(channel_id)
-                    if channel:
-                        messages_to_delete = []
-                        async for message in channel.history(limit=None):
-                            time_diff = datetime.now().astimezone() - message.created_at
-                            if (
-                                time_diff > timedelta(minutes=self.timer)
-                                and time_diff < timedelta(days=14)
-                                and len(message.attachments) > 0
-                            ):
-                                messages_to_delete.append(message)
-                                if len(messages_to_delete) >= 100:
-                                    await channel.delete_messages(messages_to_delete)
-                                    deleted_messages_count += len(messages_to_delete)
-                                    messages_to_delete = []
-                        if (
-                            messages_to_delete
-                        ):  # if there are leftover messages less than 100
+                await self.delete_messages()
+            await asyncio.sleep(self.timer * 60)
+
+    async def delete_messages(self):
+        deleted_messages_count = 0
+        # for channel_id in self.read_channel_ids(): For use with json, we will use the hardcoded list for now
+        for channel_id in [1020933443179253800, 1130587895665807371]:
+            channel = self.bot.get_channel(channel_id)
+            if channel:
+                messages_to_delete = []
+                async for message in channel.history(limit=None):
+                    time_diff = datetime.now().astimezone() - message.created_at
+                    if (
+                        time_diff > timedelta(minutes=self.timer)
+                        and time_diff < timedelta(days=14)
+                        and len(message.attachments) > 0
+                    ):
+                        messages_to_delete.append(message)
+                        if len(messages_to_delete) >= 100:
                             await channel.delete_messages(messages_to_delete)
                             deleted_messages_count += len(messages_to_delete)
-                        await channel.send(
-                            f"FlashDelete Cleanup Completed: Deleted {deleted_messages_count} messages."
-                        )
-            await asyncio.sleep(self.timer * 60)
+                            messages_to_delete = []
+                if messages_to_delete:
+                    await channel.delete_messages(messages_to_delete)
+                    deleted_messages_count += len(messages_to_delete)
+                print(
+                    "FlashDelete Cleanup Completed: Deleted {deleted_messages_count} messages."
+                )
+                await channel.send(
+                    f"FlashDelete Cleanup Completed: Deleted {deleted_messages_count} messages."
+                )
