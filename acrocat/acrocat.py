0fa7ff5c70a78cef2ef988a62aadaac60000c7e7
ropeadope62 <davechadwick@gmail.com>
2024-01-23 06:15:16 -0500

    logo file and more progress in core functionality

--- acrocat/acrocat.py
+++ acrocat/acrocat.py
@@ -3,6 +3,7 @@ from redbot.core import commands
 import random
 import asyncio
 import string
+import os
 
 class AcroCat(commands.Cog):
     def __init__(self, bot):
@@ -14,19 +15,27 @@ class AcroCat(commands.Cog):
     @commands.command()
     async def acrocat(self, ctx: commands.Context):
         self.current_acronym = self.generate_acronym()
-        embed = discord.Embed(title="Acrocat - The Best Acro Cog for Discord.", description=f"Your acronym is: `{self.current_acronym}`")
-        await ctx.send(embed=embed)
+        embed = discord.Embed(title="Acrocat - The Cat's Ass of Acro Cogs. Period.", description=f"Your acronym is: `{self.current_acronym}`")
+        
+        # Get the path to the image file
+        image_path = os.path.join(os.path.dirname(__file__), "acrocat_logo.png")
+        
+        # Set the image in the embed
+        embed.set_image(url="attachment://acrocat_logo.png")
+        
+        # Send the embed with the image
+        await ctx.send(embed=embed, file=discord.File(image_path, "acrocat_logo.png"))
+        
         await self.start_voting(ctx)
         await asyncio.sleep(30)
         
 
+    @staticmethod
     def generate_acronym():
         characters = []
         for _ in range(random.randint(3, 6)):
-            characters.append(random.choice(string.ascii_letters))
+            characters.append(random.choice(string.ascii_uppercase))
         return characters
-
-    acronym_template = generate_acronym()
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -42,10 +51,13 @@ class AcroCat(commands.Cog):
                 
     
     async def start_voting(self, ctx):
+        # Creating an embed with all the responses
         embed = discord.Embed(title="Acrocat - Current Entries")
-        for index, acronym in enumerate(self.responses.values(), start=1):
-            embed.add_field(name=f"{index}.", value=acronym, inline=False)
-            message = await ctx.send(embed=embed)
+        for index, (author, acronym) in enumerate(self.responses.items(), start=1):
+            embed.add_field(name=f"{index}.", value=f"{acronym} by {author.display_name}", inline=False)
+        
+        # Sending the embed only once after it's fully populated
+        voting_message = await ctx.send(embed=embed)
 
         def check(message):
             return message.author == ctx.author and message.channel == ctx.channel
@@ -53,15 +65,19 @@ class AcroCat(commands.Cog):
         try:
             response = await self.bot.wait_for('message', check=check, timeout=30)
             vote = int(response.content)
-            if vote in range(1, len(self.responses) + 1):
-                if vote in self.votes:
-                    self.votes[vote] += 1
-                else:
-                    self.votes[vote] = 1
-                await ctx.send(f"{message.author}'s vote has been recorded.")
+            if 1 <= vote <= len(self.responses):
+                voted_author = list(self.responses.keys())[vote - 1]
+                self.votes[voted_author] = self.votes.get(voted_author, 0) + 1
+                await ctx.send(f"{response.author.display_name}'s vote has been recorded.")
             else:
-                await ctx.send("Invalid vote. Please enter a number between 1 and {len(self.responses)}.")
+                await ctx.send(f"Invalid vote. Please enter a number between 1 and {len(self.responses)}.")
         except asyncio.TimeoutError:
             await ctx.send("Voting time has expired.")
-
-   
+        
+        # Creating an embed with all the votes
+        embed = discord.Embed(title="Acrocat - Current Votes")
+        for index, (author, votes) in enumerate(self.votes.items(), start=1):
+            embed.add_field(name=f"{index}.", value=f"{author.display_name}: {votes}", inline=False)
+        await ctx.send(embed=embed)
+        
+    
