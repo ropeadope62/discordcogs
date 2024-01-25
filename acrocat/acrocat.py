edc005e1382b97a8e9ba9e2787dbb39fd5c910b4
ropeadope62 <davechadwick@gmail.com>
2024-01-25 02:00:27 -0500

    Update acrocat.py

--- acrocat/acrocat.py
+++ acrocat/acrocat.py
@@ -18,6 +18,7 @@ class AcroCat(commands.Cog):
         self.game_state = None
         self.voting_channel = None
 
+
     async def start_voting(self, ctx):
         print(f'starting voting in {ctx.channel}')
         self.voting_channel = ctx.channel
@@ -32,13 +33,13 @@ class AcroCat(commands.Cog):
             if self.name_with_acro == 1:
                 print(f'adding {index} {response} by {author.display_name}')
                 embed.add_field(name=f"{index}.", value=f"{response} by {author.display_name}", inline=False)
-            embed.add_field(name=f"{index}.", value=f"{response}", inline=False)
+            else:
+                embed.add_field(name=f"{index}.", value=f"{response}", inline=False)
         voting_message = await ctx.send(embed=embed)
         self.voting_message_id = voting_message.id
-        print(f'storing voting message id {self.voting_message_id}')   
+        print(f'storing voting message id {self.voting_message_id}')
         print(f'storing voting channel {self.voting_channel}')
         self.responses = {}
-
 
     @commands.group()
     async def acrocat(self, ctx: commands.Context):
@@ -49,7 +50,7 @@ class AcroCat(commands.Cog):
                 title="Acrocat - The Cat's Ass of Acro Cogs.",
                 description=f"Your acronym is: **`{self.current_acronym}`**",
             )
-            
+
             image_path = os.path.join(os.path.dirname(__file__), "acrocat_logo.png")
             embed.add_field(
                 name="About",
@@ -60,31 +61,27 @@ class AcroCat(commands.Cog):
                 value="If you liked this, check out my other cogs! https://github.com/ropeadope62/discordcogs",
             )
             embed.set_image(url="attachment://acrocat_logo.png")
-            
-            message = await ctx.send(
-                embed=embed, file=discord.File(image_path, "acrocat_logo.png")
-            )
-            
+
+            message = await ctx.send(embed=embed, file=discord.File(image_path, "acrocat_logo.png"))
+
             for i in range(30, 0, -1):
                 await asyncio.sleep(1)
                 embed.description = f"Your acronym is: **`{self.current_acronym}`**\nCountdown: {i}"
                 await message.edit(embed=embed)
-            
+
             await self.start_voting(ctx)
 
     @staticmethod
     def generate_acronym():
-        return "".join(
-            random.choice(string.ascii_uppercase) for _ in range(random.randint(3, 6))
-        )
+        return "".join(random.choice(string.ascii_uppercase) for _ in range(random.randint(3, 6)))
 
     @acrocat.command(name="letters")
     @commands.has_permissions(manage_guild=True)
     @commands.is_owner()
     async def set_letter_limits(self, ctx, min_length: int, max_length: int):
         if 1 <= min_length <= max_length:
-            self.min_length = min_length
-            self.max_length = max_length
+            self.min_acro_length = min_length
+            self.max_acro_length = max_length
             await ctx.send(f"Acronym length limits set to {min_length}-{max_length}.")
         else:
             await ctx.send(
@@ -92,46 +89,9 @@ class AcroCat(commands.Cog):
             )
 
     @acrocat.command(name="endvote")
-    @commands.has_permissions(manage_messages=True)  
+    @commands.has_permissions(manage_messages=True)
     async def end_vote_command(self, ctx):
         await self.end_vote(ctx)
-    
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author.bot or message.channel != self.voting_channel:
-            return
-
-        if self.game_state == 'collecting':
-            print(f'game state is {self.game_state}')
-            is_valid_acronym = (
-                self.current_acronym
-                and "".join(word[0].lower() for word in message.content.split() if word) == self.current_acronym.lower()
-            )
-
-            if is_valid_acronym:
-                print(f'found valid acronym from {message.author}')
-                self.responses[message.author] = message.content
-                try:
-                    await message.delete()  
-                    print(f'Deleting message from {message.author}')
-                except discord.Forbidden:
-                    print("Bot does not have permissions to delete messages.")
-                except discord.HTTPException:
-                    print("Deleting the message failed.")
-
-        elif self.game_state == 'voting':
-            print(f'game state is {self.game_state}')
-            try:
-                vote = int(message.content.strip())
-                if 1 <= vote <= len(self.responses):
-                    print(f'determined vote is {vote}')
-                    print(f'vote from {message.author} for {vote}')
-                    self.votes[message.author] = vote
-                    await message.add_reaction("✅")
-                    await message.delete()
-            except ValueError:
-                print("Deleting the message failed.")
-
 
     async def end_vote(self, ctx):
         if self.game_state != 'voting':
@@ -158,3 +118,41 @@ class AcroCat(commands.Cog):
         self.current_acronym = None
         self.responses = {}
         self.votes = {}
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot or message.channel != self.voting_channel:
+            return
+
+        if self.game_state == 'collecting':
+            print(f'game state is {self.game_state}')
+            is_valid_acronym = (
+                self.current_acronym
+                and "".join(word[0].lower() for word in message.content.split() if word) == self.current_acronym.lower()
+            )
+
+            if is_valid_acronym:
+                print(f'found valid acronym from {message.author}')
+                self.responses[message.author] = message.content
+                try:
+                    await message.delete()                   
+                    print(f'Deleting message from {message.author}')
+                except discord.Forbidden:
+                    print("Bot does not have permissions to delete messages.")
+                except discord.HTTPException:
+                    print("Deleting the message failed.")
+
+        elif self.game_state == 'voting':
+            print(f'game state is {self.game_state}')
+            try:
+                vote = int(message.content.strip())
+                if 1 <= vote <= len(self.responses):
+                    print(f'determined vote is {vote}')
+                    print(f'vote from {message.author} for {vote}')
+                    self.votes[message.author] = vote
+                    await message.add_reaction("✅")
+                    await message.delete()
+            except ValueError:
+                print("Deleting the message failed.")
+
+
