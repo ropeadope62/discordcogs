cbb818eeb1ccb2a12846a348581adb36c9c44451
ropeadope62 <davechadwick@gmail.com>
2024-02-11 13:15:39 -0500

    Update spectre.py

--- spectre/spectre.py
+++ spectre/spectre.py
@@ -12,18 +12,18 @@ class Spectre(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         for guild in self.bot.guilds:
-            self.invite_cache[guild.id] = await guild.invites()
+            self.invite_cache[guild.id] = {invite.code: invite for invite in await guild.invites()}
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
         guild = member.guild
         invites_before_join = self.invite_cache.get(guild.id)
-        invites_after_join = await guild.invites()
+        invites_after_join = {invite.code: invite for invite in await guild.invites()}
         invite = None
 
-        for invite_before in invites_before_join:
-            for invite_after in invites_after_join:
-                if invite_before.code == invite_after.code and invite_before.uses < invite_after.uses:
+        for code, invite_before in invites_before_join.items():
+            if code in invites_after_join:
+                if invite_before.uses < invites_after_join[code].uses:
                     invite = invite_before
                     break
 
@@ -32,13 +32,14 @@ class Spectre(commands.Cog):
             channel = self.bot.get_channel(1202968017785585705)  # Replace with your channel ID
             await channel.send(f"{member.display_name} JOINED WITH {invite.code}. THE INVITE WAS CREATED BY {inviter.display_name}")
 
-        invite_data = {
-            "member": member.display_name,
-            "invite_code": invite.code,
-            "inviter": inviter.display_name
-        }
-        with open('spectre_data.json', 'w') as json_file:
-            json.dump(invite_data, json_file)
+            invite_data = {
+                "member": member.display_name,
+                "invite_code": invite.code,
+                "inviter": inviter.display_name
+            }
+            with open('spectre_data.json', 'a') as json_file:  
+                json.dump(invite_data, json_file)
+                json_file.write('\n')  
 
 async def setup(bot):
     await bot.add_cog(Spectre(bot))
