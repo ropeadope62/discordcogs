af67483cec2487a73c8a2d324473f298d3b61fc0
ropeadope62 <davechadwick@gmail.com>
2024-07-15 23:04:27 -0400

    Update bullshido_ai.py

--- bullshido/bullshido_ai.py
+++ bullshido/bullshido_ai.py
@@ -18,7 +18,7 @@ def generate_hype(user_config, attacker_id, defender_id):
     
 
     prompt = f"""
-    Given the following user config, {attacker_data} and {defender_data}, Generate some hype about the upcoming match. Keep your responses under 300 characters. Include some of their stats in the response.  
+    Given the following user config, {attacker_data} and {defender_data}, Generate some hype about the upcoming match. Keep your responses under 500 characters. Include some of their stats in the response.  
         """
 
     response = client.chat.completions.create(model="gpt-4",
