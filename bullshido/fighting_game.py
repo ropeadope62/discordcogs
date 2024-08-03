28be5ab29eb3a6f6b4954dc672608ed138781393
ropeadope62 <davechadwick@gmail.com>
2024-08-03 01:28:20 -0400

    Update fighting_game.py

--- bullshido/fighting_game.py
+++ bullshido/fighting_game.py
@@ -832,7 +832,6 @@ class FightingGame:
                 color=0xFF0000
             )
             file = discord.File(fight_image_path, filename="fight_image.png")
-            embed.set_image(url="attachment://fight_image.png")
             self.embed_message = await self.channel.send(file=file, embed=embed)
             await asyncio.sleep(15)
 
